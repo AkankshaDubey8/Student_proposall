d2daf8d2aa4c1d53b2f6a7119c752827f2ae1f6e
AkankshaDubey8 <akankshadubey0088@gmail.com>
2026-05-06 14:15:50 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://your-backend-link.onrender.com";
 
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
   e.preventDefault();
